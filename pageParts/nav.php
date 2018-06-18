84dcc9f567893cb08d7147a1c9d8493695b933de
stevreeper <38439416+stevreeper@users.noreply.github.com>
2018-06-18 00:28:49 -0300

    Nav bar em progresso

---
+++ pageParts/nav.php
@@ -0,0 +1,31 @@
+  <a class="navbar-brand" href="#" data-toggle="collapse" data-target="#navbarNavDropdown"><img src="imagens/logo_intermedio.png" width="50" height="50"> Qualitas</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link <?php if($_SESSION['page'] == "home"){echo "active";}?>">Home Page</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle <?php if($_SESSION['page'] == "imoveis"){echo "active";}?>" href="#" id="navbarDropdownImoveis" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Imóveis</a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownImoveis">
+          <a class="dropdown-item badge-dark" href="#">Registrar imóveis</a>
+          <a class="dropdown-item badge-dark" href="#">Alterar imóveis</a>
+          <a class="dropdown-item badge-dark" href="#">Excluir imóveis</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link <?php if($_SESSION['page'] == "imobiliarias"){echo "active";}?>" href="#">Imobiliárias</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle <?php if($_SESSION['page'] == "pagamentos"){echo "active";}?>" href="#" id="navbarDropdownPagamentos" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Pagamentos
+        </a>
+        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownPagamentos">
+          <a class="dropdown-item badge-dark" href="#">Registrar pagamentos</a>
+          <a class="dropdown-item badge-dark" href="#">Listar pagamentos</a>
+        </div>
+      </li>
+    </ul>
+  </div>
