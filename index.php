e1955f99162d4fa9cddb5dc5e931d8693e9fffbd
stevreeper <38439416+stevreeper@users.noreply.github.com>
2018-06-19 00:05:16 -0300

    Layout index pronto e imoveis ainda vazio

--- index.php
+++ index.php
@@ -3,16 +3,21 @@
 
 <head>
     <title>Qualitas</title>
+    <!--Add icon-->
+    <link rel="icon" type="image/png" href="imagens/logo_pequena.png" sizes="32X32">
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1" shrink-to-fit=no>
+    <!--Add Bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB"
         crossorigin="anonymous">
+    <!--Add jquery-->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
         crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T"
         crossorigin="anonymous"></script>
+    <!--Add css-->
     <link rel="stylesheet" href="css/index.css">
 </head>
 
@@ -25,22 +30,62 @@
     ?>
 </nav>
     <main>
-      <div class="bg-dark block1 container">
-            <img class="" src="imagens/logo_texto.png" alt="Logo Qualitas Imobiliária e construtora">
+      <!--Bloco 1-->
+      <div class="block1 bg-dark">
+          <img id="img" class="img-fluid" width="100%" src="imagens/logo_texto.png" alt="Qualitas Imobiliária e Construtora">
       </div>
-        <div class="block2">
+      <!--Bloco 2-->
+      <div class="block2 bg-light">
+        <div class="container">
+          <div class="row acesso">
+              <h1>Acesso rápido</h1>
+          </div>
           <div class="row">
-            <div class="col card">
-                oi
+            <div class="col">
+              <div class="card">
+                <div class="card-header">
+                  Imóveis
+                </div>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Imóvel 1</li>
+                    <li class="list-group-item">Imóvel 2</li>
+                    <li class="list-group-item">Imóvel 3</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div class="col card">
-                oi
+            <div class="col">
+              <div class="card">
+                <div class="card-header">
+                  Imóveis
+                </div>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Imóvel 1</li>
+                    <li class="list-group-item">Imóvel 2</li>
+                    <li class="list-group-item">Imóvel 3</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div class="col card">
-                oi
+            <div class="col">
+              <div class="card">
+                <div class="card-header">
+                  Imóveis
+                </div>
+                <div class="card-body">
+                  <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Imóvel 1</li>
+                    <li class="list-group-item">Imóvel 2</li>
+                    <li class="list-group-item">Imóvel 3</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
-        </div
+        </div>
+      </div>
     </main>
 </body>
 
--- pageParts/nav.php
+++ pageParts/nav.php
@@ -1,19 +1,12 @@
-  <a class="navbar-brand" href="#"><img src="imagens/logo_intermedio.png" width="50" height="50"> Qualitas</a>
+  <a class="navbar-brand" href="<?php if($_SESSION['page'] == "home"){echo "index.php";}else{echo "../index.php";}?>"><img src="<?php if($_SESSION['page'] == "home"){echo "imagens/logo_intermedio.png";}else{echo "../imagens/logo_intermedio.png";}?>" width="50" height="50"> Qualitas</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavDropdown">
     <ul class="navbar-nav">
+      <!--Pagina com lista de imoveis e opcao para adicionar, dentro de cada imovel opcao para excluir ou alterar-->
       <li class="nav-item">
-        <a class="nav-link <?php if($_SESSION['page'] == "home"){echo "active";}?>">Home Page</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle <?php if($_SESSION['page'] == "imoveis"){echo "active";}?>" href="#" id="navbarDropdownImoveis" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Imóveis</a>
-        <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownImoveis">
-          <a class="dropdown-item badge-dark" href="#">Registrar imóveis</a>
-          <a class="dropdown-item badge-dark" href="#">Alterar imóveis</a>
-          <a class="dropdown-item badge-dark" href="#">Excluir imóveis</a>
-        </div>
+        <a class="nav-link <?php if($_SESSION['page'] == "imoveis"){echo "active";}?>" href="#">Imóveis</a>
       </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle <?php if($_SESSION['page'] == "imobiliarias"){echo "active";}?>" href="#" id="navbarDropdownImobiliarias" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Imobiliárias</a>
