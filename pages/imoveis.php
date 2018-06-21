35d04d55b0f579c508c79ba2c40961e857e30be9
stevreeper <38439416+stevreeper@users.noreply.github.com>
2018-06-21 15:05:48 -0300

    Botao cancelar e formulario funcionando, falta botao salvar

--- pages/imoveis.php
+++ pages/imoveis.php
@@ -46,13 +46,13 @@
               </div>
               <!-- Modal body -->
               <div class="modal-body" id="modalBody">
-                <form id="cadastrarImovel" action="../php/cadastrarImoveis.php" method="post">
+                <form id="cadastrarImovel">
                   <!-- Form imagem -->
                   <div class="container">
                     <div class="row" id="croppieDiv">
                       <div class="input-group mb-3">
                         <div class="custom-file">
-                          <input type="file" accept="image/*" class="custom-file-input" id="inputImagem" name="imagemInput">
+                          <input type="file" accept="image/*" class="custom-file-input" id="inputImagem">
                           <label class="custom-file-label" for="inputImagem">Escolha uma imagem</label>
                         </div>
                       </div>
@@ -144,7 +144,7 @@
               <!-- Modal footer -->
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" id="btnCancelar" data-dismiss="modal">Cancelar</button>
-                <button type="submit" form="cadastrarImovel" class="btn btn-success" >Salvar</button>
+                <button type="submit" form="cadastrarImovel" class="btn btn-success" id="btnSalvar" >Salvar</button>
               </div>
             </div>
           </div>
@@ -153,6 +153,7 @@
     </div>
   </main>
   <script type="text/javascript">
+
     // Pegar imagem
     $("#inputImagem").change(() => {
       if(inputImagem.files[0]){
@@ -202,8 +203,92 @@
 
     //Button Cancelar
     $("#btnCancelar").click(()=>{
+      //Limpar form
       $("#cadastrarImovel")[0].reset();
+
+      //Limpar div
+      let divCroppie = document.getElementById("croppieDiv");
+      while (divCroppie.hasChildNodes()) {
+          divCroppie.removeChild(divCroppie.lastChild);
+      }
+
+      //Criar elementos
+      divCroppie.className = "row";
+      let divGroup = document.createElement("div");
+      divGroup.className = "input-group mb-3";
+      let divCustomFile = document.createElement("div");
+      divCustomFile.className = "custom-file";
+      let inputImg = document.createElement("input");
+      inputImg.setAttribute('type', 'file');
+      inputImg.setAttribute ('accept', "image/*");
+      inputImg.className = "custom-file-input";
+      inputImg.id = "inputImagem";
+      let labelImg = document.createElement("label");
+      labelImg.className = "custom-file-label";
+      labelImg.setAttribute('for',"inputImagem");
+      labelImg.innerHTML = "Escolha uma imagem";
+
+      //Colocar elementos na pagina
+      divCroppie.appendChild(divGroup);
+      divGroup.appendChild(divCustomFile);
+      divCustomFile.appendChild(inputImg);
+      divCustomFile.appendChild(labelImg);
+
+      // Pegar imagem
+      $("#inputImagem").change(() => {
+        if(inputImagem.files[0]){
+          let uploadImg = $("#croppieDiv").croppie({
+            viewport: {
+              width: 400,
+              height: 250,
+              type: 'square'
+            },
+            boundary: {
+              width: 500,
+              height: 500
+            }
+          });
+          //Ler upload
+          let reader = new FileReader();
+          reader.onload = (img) => {
+            uploadImg.croppie('bind',{
+              url: img.target.result
+            })
+            //Criar button
+            let btnResult = document.createElement("button");
+            btnResult.className = "btn btn-dark col-12 btnresult";
+            btnResult.innerHTML = "Concluir";
+            $("#croppieDiv").append(btnResult);
+            //Função do botão
+            $(btnResult).click(()=>{
+              uploadImg.croppie('result', 'blob').then((blob)=>{
+                var imgBlob = blob;
+              })
+              uploadImg.croppie('result', 'base64').then((base64)=>{
+                //Limpar div e mostrar imagem
+                let divCroppie = document.getElementById("croppieDiv");
+                while (divCroppie.hasChildNodes()) {
+                    divCroppie.removeChild(divCroppie.lastChild);
+                }
+                let img = document.createElement("img");
+                img.className = "rounded img-fluid mx-auto d-block"
+                img.src = base64;
+                divCroppie.appendChild(img);
+              })
+            })
+          }
+          reader.readAsDataURL(inputImagem.files[0]);
+        }
+      })
     })
+
+    //Button Salvar
+      let form = document.getElementById("cadastrarImovel");
+      form.addEventListener('submit', ()=>{
+        if(form.checkValidity()){
+
+        }
+      })
   </script>
 </body>
 
