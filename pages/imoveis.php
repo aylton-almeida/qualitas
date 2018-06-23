af423860b705cf49b88b505ec29e52d02ed3f3e9
stevreeper <38439416+stevreeper@users.noreply.github.com>
2018-06-23 02:04:19 -0300

    cadastrarimoveis funcionando, envio de data para ele problematico

--- pages/imoveis.php
+++ pages/imoveis.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt_br">
 
 <head>
   <title>Imóveis</title>
@@ -9,15 +9,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" shrink-to-fit=no>
   <!--Add Bootstrap-->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-  <!--Add jquery-->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-  <!--Add css-->
-  <link rel="stylesheet" href="../css/imoveis.css">
   <!-- Add cropie -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.css"/>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.js"></script>
+  <!--Add css-->
+  <link rel="stylesheet" href="../css/imoveis.css">
 </head>
 
 <body>
@@ -52,7 +47,7 @@
                     <div class="row" id="croppieDiv">
                       <div class="input-group mb-3">
                         <div class="custom-file">
-                          <input type="file" accept="image/*" class="custom-file-input" id="inputImagem">
+                          <input type="file" accept="image/*" class="custom-file-input" id="inputImagem" name="imagem" required>
                           <label class="custom-file-label" for="inputImagem">Escolha uma imagem</label>
                         </div>
                       </div>
@@ -63,24 +58,24 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text" id="nomeSpan">Nome</span>
                     </div>
-                    <input id="nomeInput" class="form-control" placeholder="Digite um nome..." type="text" aria-describedby="nomeSpan" name="nomeImovel" required>
+                    <input id="nomeInput" class="form-control" placeholder="Digite um nome..." type="text" aria-describedby="nomeSpan" name="nome" required>
                   </div>
                   <!-- Rua -->
                   <div class="input-group mb-3">
                     <div class="input-group-prepend">
                       <span class="input-group-text" id="endereçoSpan">Endereço</span>
                     </div>
-                    <input id="ruaInput" class="form-control" placeholder="Rua" type="text" aria-describedby="endereçoSpan" name="ruaImovel" required>
+                    <input id="ruaInput" class="form-control" placeholder="Rua" type="text" aria-describedby="endereçoSpan" name="rua" required>
                   </div>
                   <!-- Número, bairro e complemento -->
                   <div class="input-group mb-3">
-                    <input id="numeroInput" class="form-control" placeholder="Número" type="number" name="numeroImovel" required>
-                    <input id="bairroInput" class="form-control" placeholder="Bairro" type="text" name="bairroImovel" required>
-                    <input id="complementoInput" class="form-control" placeholder="Complemento" type="text" name="complementoImovel" required>
+                    <input id="numeroInput" class="form-control" placeholder="Número" type="number" name="numero" required>
+                    <input id="bairroInput" class="form-control" placeholder="Bairro" type="text" name="bairro" required>
+                    <input id="complementoInput" class="form-control" placeholder="Complemento" type="text" name="complemento" required>
                   </div>
                   <!-- Estado e cidade -->
                   <div class="input-group mb-3">
-                    <select class="custom-select" id="estadoInput" name="estadoImovel" required>
+                    <select class="custom-select" id="estadoInput" name="estado">
                       <option selected>Estado...</option>
                       <option value="1">AC</option>
                       <option value="2">AL</option>
@@ -110,14 +105,14 @@
                       <option value="26">SE</option>
                       <option value="27">TO</option>
                     </select>
-                    <input id="cidadeInput" class="form-control" placeholder="Digite uma cidade..." type="text" name="cidadeImovel" required>
+                    <input id="cidadeInput" class="form-control" placeholder="Digite uma cidade..." type="text" name="cidade" required>
                   </div>
                   <!-- Valor aluguel -->
                   <div class="input-group mb-3">
                     <div class="input-group-prepend">
                       <span class="input-group-text" id="precoSpan">R$</span>
                     </div>
-                    <input id="precoInput" class="form-control" placeholder="Preço aluguel" type="number" aria-describedby="precoSpan" name="precoImovel" required>
+                    <input id="precoInput" class="form-control" placeholder="Preço aluguel" type="number" aria-describedby="precoSpan" name="preco" required>
                     <div class="input-group-append">
                       <span class="input-group-text" id="precoSpan">,00</span>
                     </div>
@@ -127,7 +122,7 @@
                     <div class="input-group-prepend">
                       <span class="input-group-text" id="imobiliariaSpan">Imobiliária</span>
                     </div>
-                    <select class="custom-select" id="imobiliariaInput" aria-describedby="imobiliariaSpan" name="imobiliariaImovel">
+                    <select class="custom-select" id="imobiliariaInput" aria-describedby="imobiliariaSpan" name="imobiliaria">
                       <option selected>Escolha uma imobiliária...</option>
                       <option value="1">Conceito Empreendimentos Imobiliarias LTDA</option>
                       <option value="2">J. Fróes Imóveis LTDA</option>
@@ -140,11 +135,12 @@
                   </div>
                   <!-- <input type="submit" id="jorge"> -->
                 </form>
+                <div id="msg" class="col-sm-10 offset-sm-1" role="alert"></div>
               </div>
               <!-- Modal footer -->
               <div class="modal-footer">
                 <button type="button" class="btn btn-danger" id="btnCancelar" data-dismiss="modal">Cancelar</button>
-                <button type="submit" form="cadastrarImovel" class="btn btn-success" id="btnSalvar" >Salvar</button>
+                <button type="button" class="btn btn-success" id="btnSalvar" >Salvar</button>
               </div>
             </div>
           </div>
@@ -152,8 +148,11 @@
       </div>
     </div>
   </main>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.js"></script>
   <script type="text/javascript">
-
     // Pegar imagem
     $("#inputImagem").change(() => {
       if(inputImagem.files[0]){
@@ -182,7 +181,7 @@
           //Função do botão
           $(btnResult).click(()=>{
             uploadImg.croppie('result', 'blob').then((blob)=>{
-              var imgBlob = blob;
+              imgBlob = blob;
             })
             uploadImg.croppie('result', 'base64').then((base64)=>{
               //Limpar div e mostrar imagem
@@ -201,8 +200,8 @@
       }
     })
 
-    //Button Cancelar
-    $("#btnCancelar").click(()=>{
+    //Limpar formulário de cadastro de imóvel
+    function limpaForm(){
       //Limpar form
       $("#cadastrarImovel")[0].reset();
 
@@ -262,7 +261,7 @@
             //Função do botão
             $(btnResult).click(()=>{
               uploadImg.croppie('result', 'blob').then((blob)=>{
-                var imgBlob = blob;
+                imgBlob = blob;
               })
               uploadImg.croppie('result', 'base64').then((base64)=>{
                 //Limpar div e mostrar imagem
@@ -280,15 +279,53 @@
           reader.readAsDataURL(inputImagem.files[0]);
         }
       })
+    }
+    //Button Cancelar
+    $("#btnCancelar").click(()=>{
+      limpaForm();
     })
 
     //Button Salvar
-      let form = document.getElementById("cadastrarImovel");
-      form.addEventListener('submit', ()=>{
-        if(form.checkValidity()){
-
-        }
-      })
+      $("#btnSalvar").click(()=>{
+      if($("#cadastrarImovel")[0].checkValidity()){
+        //Fechar modalBody
+        $('#modalCadastrarImovel').modal('hide');
+        //limpar form
+        limpaForm();
+        //Criar objeto
+        let data = {
+        nome: $('#nomeInput').val(),
+        rua: $('#ruaInput').val(),
+        numero: $('#numeroInput').val(),
+        bairro: $('#bairroInput').val(),
+        complemento: $('#complementoInput').val(),
+        estado: $('#estadoInput').val(),
+        cidade: $('#cidadeInput').val(),
+        preco: $('#precoInput').val(),
+        imobiliaria: $('#imobiliariaInput').val(),
+        imagem: imgBlob
+      };
+      let json = JSON.stringify(data);
+        //Enviar form por ajax
+        $.ajax({
+          url: '../php/cadastrarImoveis.php',
+          type: "POST",
+          data: json,
+          contentType: "application/json; charset=utf-8",
+          dataType: "json",
+          processData: false,
+          contentType: false,
+          success: function (){
+            console.log("Cadastro efetuado com sucesso");
+          },
+          error: function (error){
+            console.log("Erro ao cadastrar imóvel");
+          }
+        })
+      }else{
+        $('#msg').html('<div class="alert alert-danger fade show">Preencha todos os campos corretamente!<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button></div>');
+      }
+    })
   </script>
 </body>
 
