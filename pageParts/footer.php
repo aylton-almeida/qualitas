7bf4c26807233dbe87ab4aa10fbc57fa8f36eeb5
stevreeper <38439416+stevreeper@users.noreply.github.com>
2018-06-26 18:15:13 -0300

    navbar com login pronta

--- pageParts/footer.php
+++ pageParts/footer.php
@@ -5,3 +5,24 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
 <!-- Add croppie script -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.2/croppie.js"></script>
+<!-- Add firebase -->
+<script src="https://www.gstatic.com/firebasejs/5.1.0/firebase.js"></script>
+<!-- Iniciar Firebase -->
+<script>
+  var config = {
+    apiKey: "AIzaSyD_XmxvW05XB7WrV_lwhfYn-fzTAgfAYZ4",
+    authDomain: "qualitas-24b79.firebaseapp.com",
+    databaseURL: "https://qualitas-24b79.firebaseio.com",
+    projectId: "qualitas-24b79",
+    storageBucket: "qualitas-24b79.appspot.com",
+    messagingSenderId: "347582656373"
+  };
+  firebase.initializeApp(config);
+</script>
+<!-- Add fun~c"oes do firebase -->
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase-auth.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase-database.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase-firestore.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase-messaging.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.12.1/firebase-functions.js"></script>
