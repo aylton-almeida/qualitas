1390d9173cbee8cafb0dde54013966e4b2133701
Aylton Almeida <ayltonalmeida@gmail.com>
2018-07-23 09:37:29 +0200

    navbar imobiliarias

--- pageParts/nav.php
+++ pageParts/nav.php
@@ -11,17 +11,13 @@
     <li class="nav-item">
       <a class="nav-link <?php if($_SESSION['page'] == "home"){echo "active";}?>" href="<?php if($_SESSION['page'] == "home"){echo "index.php";}else{echo "../index.php";}?>">Home</a>
     </li>
-    <!--Pagina com lista de imoveis e opção para adicionar, dentro de cada imóvel opção para excluir ou alterar-->
+    <!--Pagina com lista de imoveis -->
     <li class="nav-item">
       <a class="nav-link <?php if($_SESSION['page'] == "imoveis"){echo "active";}?>" href="<?php if($_SESSION['page'] == "home"){echo "pages/imoveis.php";}else{echo "../pages/imoveis.php";}?>">Imóveis</a>
     </li>
-    <li class="nav-item dropdown">
-      <a class="nav-link dropdown-toggle <?php if($_SESSION['page'] == "imobiliarias"){echo "active";}?>" href="#" id="navbarDropdownImobiliarias" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Imobiliárias</a>
-      <div class="dropdown-menu bg-dark" aria-labelledby="navbarDropdownImobiliarias">
-        <a class="dropdown-item badge-dark" href="#">Registrar imobiliária</a>
-        <a class="dropdown-item badge-dark" href="#">Alterar Imobiliária</a>
-        <a class="dropdown-item badge-dark" href="#">Excluir imobiliária</a>
-      </div>
+    <!--Pagina com lista de imboliárias  -->
+    <li class="nav-item">
+      <a class="nav-link <?php if($_SESSION['page'] == "imobiliarias"){echo "active";}?>" href="<?php if($_SESSION['page'] == "home"){echo "pages/imobiliarias.php";}else{echo "../pages/imobiliarias.php";}?>">Imobiliárias</a>
     </li>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle <?php if($_SESSION['page'] == "pagamentos"){echo "active";}?>" href="#" id="navbarDropdownPagamentos" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
