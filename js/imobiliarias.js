a12c60bdf9af257c1f274607fa70c6ac7975c1cf
Aylton Almeida <ayltonalmeida@gmail.com>
2018-07-26 05:36:28 +0200

    pegar imobiliarias parcialmente

--- js/imobiliarias.js
+++ js/imobiliarias.js
@@ -332,3 +332,135 @@ $("#btnSalvar").click(() => {
     mensagemModErr("Preencha todos os campos corretamente!", 1);
   }
 })
+
+//Pegar imobiliarias
+firebase.firestore().collection("imobiliarias").orderBy('nome').get()
+  .then(function(querySnapshot) {
+    querySnapshot.forEach(function(imobiliaria) {
+      //Para cada imobiliaria recuperado
+      let col = document.createElement("div");
+      col.className = "col-md-4 col-sm-6 d-flex align-items-stretch";
+      let card = document.createElement("div");
+      card.className = "card bg-dark text-light";
+      let img = document.createElement("img");
+      let imgUrl;
+      firebase.storage().ref().child(imobiliaria.data().imagem + '/Logo').getDownloadURL()
+        .then(function(url) {
+          img.className = "card-img-top";
+          img.src = url;
+          img.style.width = "100%";
+          imgUrl = url;
+        })
+        .catch(function(error) {
+          console.log(error);
+        })
+      let cardBody = document.createElement("div");
+      cardBody.className = "card-body";
+      let title = document.createElement("h5");
+      title.className = "card-title";
+      title.innerHTML = imobiliaria.data().nome;
+      let endereco = document.createElement("p");
+      endereco.className = "card-text";
+      endereco.innerHTML = imobiliaria.data().endereco.rua + ", " + imobiliaria.data().endereco.numero + "<br>" + imobiliaria.data().endereco.complemento;
+      $("#modalImovelDetalhado").before(col);
+      col.appendChild(card)
+      card.appendChild(img);
+      card.appendChild(cardBody);
+      cardBody.appendChild(title);
+      cardBody.appendChild(endereco);
+      // Click no card
+      /*$(card).click(() => {
+        //Definir atributos do imobiliaria
+        $("#Detalheimobiliaria").html(imobiliaria.data().nome + ", " + imobiliaria.data().endereco.complemento);
+        $("#imgDetalhado").attr('src', '');
+        $("#imgDetalhado").attr('src', imgUrl);
+        $('#pRua').html(imobiliaria.data().endereco.rua + ', ' + imobiliaria.data().endereco.numero);
+        $('#pComplemento').html(imobiliaria.data().endereco.complemento);
+        $('#pBairro').html('Bairro ' + imobiliaria.data().endereco.bairro);
+        $('#pCidade').html(imobiliaria.data().endereco.cidade + ' - ' + imobiliaria.data().endereco.estado);
+        $('#pPreco').html('Preço do alguel: R$' + imobiliaria.data().preco + ',00');
+        $('#pImobiliaria').html('Imobiliária responsável: ' + imobiliaria.data().imobiliaria);
+        //Maps
+        var map;
+
+        function initMap() {
+          map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+              lat: -34.397,
+              lng: 150.644
+            },
+            zoom: 16
+          });
+          geocoder = new google.maps.Geocoder();
+          codeAddress(geocoder, map);
+        }
+
+        function codeAddress(geocoder, map) {
+          geocoder.geocode({
+            'address': imobiliaria.data().endereco.rua + ', ' + imobiliaria.data().endereco.numero
+          }, function(results, status) {
+            if (status === 'OK') {
+              map.setCenter(results[0].geometry.location);
+              var marker = new google.maps.Marker({
+                map: map,
+                position: results[0].geometry.location
+              });
+            } else {
+              console.log('Geocode was not successful for the following reason: ' + status);
+            }
+          });
+        }
+
+        initMap();
+
+        $("#modalimobiliariaDetalhado").modal('toggle');
+        if (!testeAdmin) {
+          $('#modalFooter').hide();
+        }
+        // Button Alterar
+        $('#btnAlterar').click(() => {
+          // while ($('#croppieDiv').children().length > 0) {
+          //     $('#croppieDiv').children().remove();
+          // }
+          // let img = document.createElement("img");
+          // img.className = "rounded img-fluid mx-auto d-block";
+          // $(img).attr('src', imgUrl);
+          // $('#croppieDiv').append(img);
+          // $('#modalCadastrarimobiliaria').on('show.bs.modal', function() {
+          //   $('#modalimobiliariaDetalhado').modal('hide');
+          // })
+          // $('#modalCadastrarimobiliaria').modal('toggle')
+          // $('#modalCadastrarimobiliaria').modal({
+          //   focus: true
+          // })
+        })
+        // Button Excluir
+        $('#btnExcluir').click(() => {
+          showLoader();
+          //Apagar firestore
+          firebase.firestore().collection("imoveis").doc(imobiliaria.data().nome + "," + imobiliaria.data().endereco.complemento).delete().then(function() {
+            //Apagar imagens
+            firebase.storage().ref(imobiliaria.data().imagem + '/imagemCapa').delete().then(function() {
+              // Imagem apagada
+              hideLoader();
+              mensagemModSuc('Imóvel excluido com sucesso', 2);
+              setTimeout(() => {
+                window.location.href = "imoveis.php";
+              }, 2000);
+            }).catch(function(error) {
+              // Erro ao apagar imagem
+              console.log(error);
+            });
+          }).catch(function(error) {
+            //Erro removendo documento
+            console.error("Error ao remover documento: ", error);
+          });
+        })
+      })*/
+    });
+  })
+  .catch(function(error) {
+    //Nenhum imóvel encontrado
+    console.log('Nenhum imóvel cadastrado encontrado');
+    console.log(error);
+  })
